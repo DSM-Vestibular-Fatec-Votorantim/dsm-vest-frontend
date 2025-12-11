5fc418958bc70a720ab80dcda9b865c350f04d38
Felipe Thiago <felipethiagodasilva@gmail.com>
2025-12-11 17:31:43 -0300

    fix: add auth service on layout

--- app/services/authService.ts
+++ app/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_BASEURL + "/logins";
+const apiUrl = process.env.NEXT_PUBLIC_API_BASEURL + "/login";
 
 interface LoginCredentials {
   email: string;
@@ -34,41 +34,41 @@ class AuthService {
     }
   }
 
-  async forgotPassword(email: string) {
-    try {
-      const response = await axios.post(`${apiUrl}/forgot`, {email});
+  // async forgotPassword(email: string) {
+  //   try {
+  //     const response = await axios.post(`${apiUrl}/forgot`, {email});
 
-      return response.data;
-    } catch (error) {
-      console.log(error);
-      //console.error("Erro ao enviar email de recuperação:", error);
-      //throw error;
-    }
-  }
+  //     return response.data;
+  //   } catch (error) {
+  //     console.log(error);
+  //     //console.error("Erro ao enviar email de recuperação:", error);
+  //     //throw error;
+  //   }
+  // }
 
-  async verifyCode(email: string, codigo: string) {
-  try {
-    const response = await axios.post(`${apiUrl}/verify`, { email, codigo });
-    return response.data;
-  } catch (error) {
-    console.error("Erro ao verificar código:", error);
-    throw error;
-  }
-  }
+  // async verifyCode(email: string, codigo: string) {
+  // try {
+  //   const response = await axios.post(`${apiUrl}/verify`, { email, codigo });
+  //   return response.data;
+  // } catch (error) {
+  //   console.error("Erro ao verificar código:", error);
+  //   throw error;
+  // }
+  // }
 
-  async resetPassword(email: string, codigo: string, novaSenha: string) {
-    try {
-      const response = await axios.patch(`${apiUrl}/reset`, {
-        email,
-        codigo,
-        novaSenha,
-      });
-      return response.data;
-    } catch (error) {
-      console.error("Erro ao redefinir senha:", error);
-      throw error;
-    }
-  }
+  // async resetPassword(email: string, codigo: string, novaSenha: string) {
+  //   try {
+  //     const response = await axios.patch(`${apiUrl}/reset`, {
+  //       email,
+  //       codigo,
+  //       novaSenha,
+  //     });
+  //     return response.data;
+  //   } catch (error) {
+  //     console.error("Erro ao redefinir senha:", error);
+  //     throw error;
+  //   }
+  // }
 
   async getUser(tokenFromArg?: string) {
     try {
